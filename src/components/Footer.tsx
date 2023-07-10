75a15a67b3437a193718d9671f8bc144b5fc05fe
kaijengwong <kaijengwong@micron.com>
2023-07-10 19:18:09 +0800

    split url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Layout from './Layout'
+import React from 'react';
+import Layout from './Layout';
 import Link from 'next/link'
 import { useUrl } from 'nextjs-current-url';
 
 const Footer = () => {
-    const { href: currentUrl, pathname } = useUrl() ?? {};
-
+    const { href: currentUrl } = useUrl() ?? {};
+    const testurl = currentUrl?.split('//')[1].split('/')[0];
     return (
         <footer className='myfooter w-full border-t-2 border-solid border-dark font-medium text-lg'>
             <Layout >
                 <div className='py-8 flex items-center justify-between'>
-                <span>{new Date().getFullYear()} &copy; {currentUrl}</span>
+                <span>{new Date().getFullYear()} &copy; {testurl}</span>
 
                 </div>
                
