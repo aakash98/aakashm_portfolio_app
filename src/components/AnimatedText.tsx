7347d2dc2637a6411e41d3dad6d323c5a63edb05
kaijengwong <kaijengwong@micron.com>
2023-07-11 20:19:58 +0800

    Add profile pic in index

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import {motion} from "framer-motion";
 
 const AnimatedText = ({text, className=""}:{text: string, className: string}) => {
     return (
         <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
             <h1 className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}>
-                {text}
+                <span className="inline-block">
+                    {text}
+                </span>
             </h1>
-
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,63 @@
 import Image from 'next/image';
+import AnimatedText from '@/components/AnimatedText'
 import styles from 'src/styles/Home.module.css';
 import '../app/globals.css';
-
 import Link from "next/link";
+import Layout from '@/components/Layout';
+import profilePic from "../../public/my_pic.jpg";
+
 
 export default function Home() {
   return (
     
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h4 className={styles.title}>
-          My <a href="https://nextjs.org">Next.js</a> Application
-        </h4>
-
-        <p className={styles.description}>
-        </p>
-
-        <div className={styles.grid}>
-          <Link className={styles.card} href="/about"><h2>About Me</h2>
-            <br />
-            <p>Go to this page to learn more about me</p>
-          </Link>
-
-          <Link className={styles.card} href="/experiences"><h2>My Experiences</h2>
-            <br />
-            <p>Go to this page to learn about my Experiences</p>
-          </Link>
-
-          <Link className={styles.card} href=""><h2>Latest Movies</h2>
-            <br />
-            <p>Go to this page to see the latest Movie Lists</p>
-          </Link>
-
-          <Link className={styles.card} href="/contact"><h2>My Contact</h2>
-            <br />
-            <p>Go to this page to Contact Me</p>
-          </Link>
-
-          <Link className={styles.card} href="/sorting"><h2>Sorting</h2>
-            <br />
-            <p>Go to this page to learn about Sorting Algorithm</p>
-          </Link>
-
-          <Link className={styles.card} href="https://kaijeng.netlify.app/"><h2>My Netlify App</h2>
-            <br />
-            <p>Go to this page to learn about my Netlify Portfolio</p>
-          </Link>
-
-        </div>
+    <div className='{styles.container}'>
+      <main className='flex items-center text-dark w-full min-h-screen xl:p-24 '>
+        <Layout >  
+          <div className="flex items-center text-dark w-full min-h-screen">
+            <div className='w-1/2'>
+              <Image src={profilePic} alt="Kaijeng" className='w-full h-auto'></Image>
+            </div>
+            <div className='w-1/2 flex flex-col items-center self-center'>
+              <AnimatedText text="Turning Ideas into Code" className='!text-6xl !text-left' />
+              <p>
+                As a Software Engineer, I am dedicated to turn ideas into creative web applications. Explore my projects and experiences, showcasing my various fields in Software Development.
+              </p>
+            </div>
+          </div>
+        </Layout>
       </main>
+      
+      <div className={styles.grid}>
+        <Link className={styles.card} href="/about"><h2>About Me</h2>
+          <br />
+          <p>Go to this page to learn more about me</p>
+        </Link>
 
+        <Link className={styles.card} href="/experiences"><h2>My Experiences</h2>
+          <br />
+          <p>Go to this page to learn about my Experiences</p>
+        </Link>
+
+        <Link className={styles.card} href=""><h2>Latest Movies</h2>
+          <br />
+          <p>Go to this page to see the latest Movie Lists</p>
+        </Link>
+
+        <Link className={styles.card} href="/contact"><h2>My Contact</h2>
+          <br />
+          <p>Go to this page to Contact Me</p>
+        </Link>
+
+        <Link className={styles.card} href="/sorting"><h2>Sorting</h2>
+          <br />
+          <p>Go to this page to learn about Sorting Algorithm</p>
+        </Link>
+
+        <Link className={styles.card} href="https://kaijeng.netlify.app/"><h2>My Netlify App</h2>
+          <br />
+          <p>Go to this page to learn about my Netlify Portfolio</p>
+        </Link>
+      </div>
     </div>
   );
 }
