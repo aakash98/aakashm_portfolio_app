c0b30c16de3e62e28c86febcd484af229c9cb646
kaijengwong <kaijengwong@micron.com>
2023-07-14 08:17:03 +0800

    remove padding top

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Head from 'next/head';
+import AnimatedText from '@/components/AnimatedText'
 import styles from '../styles/Home.module.css';
 import '../app/globals.css';
 
@@ -12,12 +13,19 @@ export default function Contact() {
       </Head>
       <div className={styles.container}>
         <main className={styles.main}>
-          <h4 className={styles.title}>
-          </h4>
-
-          <div className={styles.grid}>
-            
+          <div className="z-0 inline-block h-full w-full bg-light p-32 dark:bg-dark xl:p-24 lg:p-16 
+                          md:p-12 sm:p-8 pt-16">
+              <AnimatedText text="Contact Me" className='!text-6xl' />
+              <div className="grid w-full grid-cols-8 gap-16">
+                <div className="col-span-3 flex flex-col items-start justify-start">
+                  <h2 className="mb-4 text-lg font-bold uppercase text-dark/75"> test</h2>
+                </div>
+              </div>
+              <p className='font-medium'>Hi, I am Kai Jeng, I am just another Software Engineer
+              </p>
           </div>
+          
+              
         </main>
       </div>
     </div>
