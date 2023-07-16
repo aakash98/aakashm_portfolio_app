f77a1492ab31359df521f039c908ba91fca54833
kaijengwong <kaijengwong@micron.com>
2023-07-16 16:38:47 +0800

    Add Layout for other page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import '@/styles/styles.css'
+import Head from 'next/head';
+import Favicon from '../components/Favicon';
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const Layout = ({ children }: LayoutProps) => (
-  <div className="layouthome">{children}</div>
+  
+  <div className="layouthome">
+  <Head>
+    <title>Kai Jeng | Portfolio</title>
+    <meta name="description" content="index"></meta>
+    <Favicon/>
+  </Head>
+  {children}</div>
 )
 export default Layout
--- src/pages/experiences.tsx
+++ src/pages/experiences.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import {motion, useScroll} from "framer-motion";
 import '../app/globals.css';
 import { PositionType } from "@/util/ExperienceModel"
+import Layout from '@/components/Layout';
 
 const Details = ({position, company, companysite, time, work, work2, work3}:{position: string, company: string, companysite: string, time:string, work: string, work2: string, work3: string}) =>
 {
@@ -49,58 +50,60 @@ export default function Experiences() {
   return (
     <div className=''>
       <div className={styles.container}>
-        <main className={styles.main}>
-        <AnimatedText text="Experiences" className='!text-6xl' />
-          <div className='w-[75%] mx-auto relative'>
-          <div className="absolute left-12 top-0 h-full bg-dark origin-top ">
-              <Image
-                className="my-8 first:mt-0 last:mb-0 flex-col"
-                src="/ars.jpg"
-                width={50}
-                height={50}
-                alt="ars"
-              />          
-              <Details position="Software Developer" company="Alpha Red Solutions" companysite="https://www.alphareds.com/" time="2019-2022" 
-              work="Maintaining Multiple CMS Applications Including Hotel/Travel Applications"
-              work2="Migration of a decade Legacy project in Collaboration with Adobe Experience Cloud"
-              work3="Kickoff of A universal e-wallet service In Collaboration with U Mobile"/>
-          
-              <Image
-                className="my-8 first:mt-0 last:mb-0 flex-col"
-                src="/micron.png"
-                width={75}
-                height={75}
-                alt="micron"
-              />          
-              <Details position="Software Engineer" company="Micron Technology" companysite="https://www.micron.com/" time="2022-Current"
-               work="Maintaining the Micron Backend Advanced Scheduler (BEMAS) Application which focusing on Planning and Scheduling"
-               work2="Migrating Applications from VMs to Cloud using Tools for Containerization such as Docker and Openshift"
-               work3="Migrating API Gateway from WSO2 to Apigee"/>
-               </div>
-          </div>
-        </main>
+        <Layout>  
+          <main className={styles.main}>
+          <AnimatedText text="Experiences" className='!text-6xl' />
+            <div className='w-[75%] mx-auto relative'>
+            <div className="absolute left-12 top-0 h-full bg-dark origin-top ">
+                <Image
+                  className="my-8 first:mt-0 last:mb-0 flex-col"
+                  src="/ars.jpg"
+                  width={50}
+                  height={50}
+                  alt="ars"
+                />          
+                <Details position="Software Developer" company="Alpha Red Solutions" companysite="https://www.alphareds.com/" time="2019-2022" 
+                work="Maintaining Multiple CMS Applications Including Hotel/Travel Applications"
+                work2="Migration of a decade Legacy project in Collaboration with Adobe Experience Cloud"
+                work3="Kickoff of A universal e-wallet service In Collaboration with U Mobile"/>
+            
+                <Image
+                  className="my-8 first:mt-0 last:mb-0 flex-col"
+                  src="/micron.png"
+                  width={75}
+                  height={75}
+                  alt="micron"
+                />          
+                <Details position="Software Engineer" company="Micron Technology" companysite="https://www.micron.com/" time="2022-Current"
+                work="Maintaining the Micron Backend Advanced Scheduler (BEMAS) Application which focusing on Planning and Scheduling"
+                work2="Migrating Applications from VMs to Cloud using Tools for Containerization such as Docker and Openshift"
+                work3="Migrating API Gateway from WSO2 to Apigee"/>
+                </div>
+            </div>
+          </main>
 
-        <main className={styles.main}>
-          <div className='my-10'>
-          <AnimatedText text="Education" className='!text-6xl' />
-            <div className='w-[75%] mx-auto relative '>         
-              <div className="absolute left-12 top-0 h-full bg-dark origin-top ">
-              <Image
-                    className="my-8 first:mt-0 last:mb-0 flex-col"
-                    src="/monash.png"
-                    width={50}
-                    height={50}
-                    alt="monash"
-                  />          
-                <EducationDetails course="Bachelor of Computer Science" institution="Monash University" time="2015-2018" 
-                sidecourse="Data Analytics"
-                sidecourse3="Modelling for Data Analysis"
-                sidecourse2="Algorithms and Data Structures"/>
-                </div>  
-            </div>  
-          </div>
-        
-        </main>
+          <main className={styles.main}>
+            <div className='my-10'>
+            <AnimatedText text="Education" className='!text-6xl' />
+              <div className='w-[75%] mx-auto relative '>         
+                <div className="absolute left-12 top-0 h-full bg-dark origin-top ">
+                <Image
+                      className="my-8 first:mt-0 last:mb-0 flex-col"
+                      src="/monash.png"
+                      width={50}
+                      height={50}
+                      alt="monash"
+                    />          
+                  <EducationDetails course="Bachelor of Computer Science" institution="Monash University" time="2015-2018" 
+                  sidecourse="Data Analytics"
+                  sidecourse3="Modelling for Data Analysis"
+                  sidecourse2="Algorithms and Data Structures"/>
+                  </div>  
+              </div>  
+            </div>
+          
+          </main>
+        </Layout>  
       </div>
     </div>
   );
