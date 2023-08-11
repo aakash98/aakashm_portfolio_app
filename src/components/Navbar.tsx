39a4f8c012f93d692e434e588e1ae7b8795e1281
kaijengwong <kaijengwong@micron.com>
2023-08-11 10:14:32 +0800

    enhance burger menu transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,9 +20,9 @@ const Navbar = () => {
       dark:text-light relative'>
 
         <button className='flex-col justify-center hidden lg:flex items-center' onClick={handleClick}>
-          <span className={`block w-6 h-0.5 bg-gray-500  bg-dark dark:bg-light transition-all duration-300 ease-in  rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : 'translate-y-0.5'}}`}></span>
-          <span className={`block w-6 h-0.5 bg-gray-500 bg-dark dark:bg-light transition-all duration-300 ease-out  rounded-sm my-0.5 ${isOpen ? 'hidden' : 'opacity-100'}`}></span>
-          <span className={`block w-6 h-0.5 bg-gray-500 bg-dark dark:bg-light transition-all duration-300 ease-out  rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}}`}></span>
+          <span className={`block w-6 h-0.5 bg-gray-500 bg-dark dark:bg-light transition-all duration-300 ease-out rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : 'translate-y-0.5'}}`}></span>
+          <span className={`block w-6 h-0.5 bg-gray-500 bg-dark dark:bg-light transition-all duration-300 ease-out opacity-100 rounded-sm my-0.5 ${isOpen ? 'hidden' : 'opacity-100'}`}></span>
+          <span className={`block w-6 h-0.5 bg-gray-500 bg-dark dark:bg-light transition-all duration-300 ease-out rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}}`}></span>
         </button> 
 
         <div className="w-full flex justify-between items-center lg:hidden">
